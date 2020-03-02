4cfaa0c82bb8af7db243ad17901cbb39d6278f20
alfonsotech <alfonsotech@nym.hush.com>
2020-03-02 17:26:36 -0400

    Adds initial tests, deletes some unused components

--- client/src/tests/Footer.test.js
+++ client/src/tests/Footer.test.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import App from './App'
+import Footer from '../Footer'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDom.render(<App />, div)
+  ReactDom.render(<Footer />, div)
 })
